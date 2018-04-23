842c2228fb60d14fd44d1d5cfd8f5952f768ba42
小蜗牛 <asln_lz@126.com>
2018-04-23 19:21:03 +0800

    w

--- static/js/index.js
+++ static/js/index.js
@@ -83,17 +83,81 @@
 
 		}, stime);  
     }
-    
 
-    $(".verificationCode").on("click" ,function (){
-        var _phone = $(".phone").val();
-        if (!_phone){
-            return $.alert("请输入手机号");
+    function dingshiqi(obj){
+		var pro = {
+			target : "",   //目标DOM元素 非JQ元素
+			timeSum : 60,  //倒计时的起始事件
+			text: "重新发送", //倒计时读秒完成后的文案
+            clickFun : "",//当前DOM 元素点击时的 执行方法
+            afterFun : "" //定时器执行完成后的方法
+		};
+
+        var  timerTask ={
+            // 倒计时开始执行 的方法
+            timerStart : function (){
+                flag = false;
+                var timer = setInterval(function (){
+                    step = step - 1;
+                    _target.text(step + "秒");
+
+                    if(step <= 0){
+                        clearInterval(timer);
+                        _target.text(pro.text);
+                        flag = true;
+                        step = pro.timeSum;
+
+                        $(pro.target).one("click" ,clickDone);
+
+                        $.isFunction (pro.afterFun) && pro.afterFun();
+                    }
+                },1000);
+            },
+            init : function (){
+                $(pro.target).one("click" ,clickDone);
+            }
         }
 
-        $.get("/api/sendSms?mobile=" + _phone + "&callback=?", function (data){
-            console.log(data);
-        })
-    })
+		$.extend(pro , obj);
 
+		var flag = true ; // 节流阀
+		var step = pro.timeSum;
+		var _target = $(pro.target);
+
+		$(pro.target).one("click" ,clickDone);
+
+        function clickDone(){
+            if(flag) {
+                pro.clickFun(timerTask);
+            }
+        }
+	}
+
+    $.dingshiqi({
+        target : ".verificationCode",
+        clickFun: function (timerTask){
+            var _phone = $(".phone").val();
+            if (!_phone || !$(".phone_ipt").val().isPhone()){
+                return $.alert("请输入正确的手机号");
+            }
+
+            $.get("/api/sendSms?mobile=" + _phone + "&callback=?", function (data){
+                console.log(data);
+                //倒计时开始
+                if( d.code != 1){
+                    timerTask.init();
+                    $.alert(d.msg);
+                    yanzhengma();
+                }	
+                if( d.code == 1){
+                    $('.dongtai_ipt').val('');
+                    timerTask.timerStart();
+                }
+            })
+        },
+        afterFun: function (){
+            yanzhengma();
+            
+        }
+    });
 })
