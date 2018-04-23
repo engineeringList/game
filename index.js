a13fb70d7cf2e1a65e9d93f2c51eebb45ec9a022
dlyt <miaosonglin@daoxila.com>
2018-04-23 19:23:44 +0800

    a

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const compress = require('koa-compress');
 const morgan = require('koa-morgan');
 const path = require('path');
 const fs = require('fs');
+const static = require('koa-static')
 const app = new Koa();
+
+// 静态文件
+app.use(static(
+  path.join( __dirname,  './static')
+));
 
 // log
 app.use(morgan('combined'));
