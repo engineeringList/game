e5d7e2a512ab1b8ef8bfdd3d13f4096de253e32f
dlyt <miaosonglin@daoxila.com>
2018-04-26 10:08:35 +0800

    a

--- router/subscribe.js
+++ router/subscribe.js
@@ -5,4 +5,5 @@ const { SubscribeCtrl } = require('../app/controller');
 module.exports = (router) => {
     router.get('/admin/subscribe', SubscribeCtrl.wave);
     router.get('/admin/subscribe/list', SubscribeCtrl.list);
+    router.get('/api/subscribe/count', SubscribeCtrl.count);
 };
