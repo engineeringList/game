9a47f700ace4bc8a171de5908a0b84bb07d75eca
dlyt <miaosonglin@daoxila.com>
2018-04-27 16:53:38 +0800

    a

--- static/js/mIndex.js
+++ static/js/mIndex.js
@@ -7,7 +7,8 @@ $(function (){
     weixinFenxiang()
     function weixinFenxiang(){
         $.get('http://www.hw.mangofun.cn/' + 'api/wx?url='+encodeURIComponent(window.location.href)+'&callback=?',function (data){
-            data = $.extend({}, data ,{"debug" : false});
+            data = $.extend({}, data ,{"debug" : true});
+            console.log(1)
             wx.config(data);
             wx.ready(function () {
                 wx.checkJsApi({
