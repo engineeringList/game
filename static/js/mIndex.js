f7e35c54ca515984d48ec2d454e106970353bd36
dlyt <miaosonglin@daoxila.com>
2018-04-27 17:39:09 +0800

    a

--- static/js/mIndex.js
+++ static/js/mIndex.js
@@ -25,13 +25,13 @@ $(function (){
 
                 // 朋友圈
                 var wxData1 = {
-                    "title": "《火王：破晓之战》官方正版手游 预热开启", // 分享标题
+                    "title": "《火王：破晓之战》正版手游 预约火热开启", // 分享标题
                     "link": location.href.split('#')[0], // 分享链接
                     "imgUrl": 'http://www.hw.mangofun.cn/images/ad_1_logo.png' // 分享图标,
                 };
                 // 好友
                 var wxData2 = {
-                    "title": "《火王：破晓之战》官方正版手游 预热开启", // 分享标题
+                    "title": "《《火王：破晓之战》正版手游 预约火热开启", // 分享标题
                     "desc": "《火王：破晓之战》邀你共赏全新幻想国风世界",
                     "link": location.href.split('#')[0], // 分享链接
                     "imgUrl": 'http://www.hw.mangofun.cn/images/ad_1_logo.png' // 分享图标,
